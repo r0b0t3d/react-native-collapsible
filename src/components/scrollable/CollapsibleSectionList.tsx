a3c469755159498461ee2ef7bb04da9b79134d9e
Tuan Luong <tuanluong.it@gmail.com>
2023-07-17 14:21:16 +0700

    feat: add sectionlist

---
+++ src/components/scrollable/CollapsibleSectionList.tsx
@@ -0,0 +1,156 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import { View, StyleSheet, SectionList, SectionListProps } from 'react-native';
+import Animated, {
+  runOnJS,
+  useAnimatedReaction,
+} from 'react-native-reanimated';
+import useAnimatedScroll from './useAnimatedScroll';
+import useInternalCollapsibleContext from '../../hooks/useInternalCollapsibleContext';
+import type { CollapsibleProps } from '../../types';
+import AnimatedTopView from '../header/AnimatedTopView';
+import useCollapsibleContext from '../../hooks/useCollapsibleContext';
+
+const AnimatedSectionList = Animated.createAnimatedComponent(SectionList);
+
+type Props<Data> = Omit<SectionListProps<Data>, 'scrollEnabled'> &
+  CollapsibleProps;
+
+export default function CollapsibleSectionList<Data>({
+  headerSnappable = true,
+  ...props
+}: Props<Data>) {
+  const { headerHeight } = useCollapsibleContext();
+  const { contentMinHeight, scrollViewRef, fixedHeaderHeight } =
+    useInternalCollapsibleContext();
+  const mounted = useRef(true);
+  const contentHeight = useRef(0);
+  const [internalContentMinHeight, setInternalContentMinHeight] = useState(
+    contentMinHeight.value
+  );
+  const [internalProgressViewOffset, setInternalProgressViewOffset] =
+    useState(0);
+
+  useEffect(() => {
+    return () => {
+      mounted.current = false;
+    };
+  }, []);
+
+  const scrollTo = useCallback((yValue: number, animated = true) => {
+    console.warn('Not supported');
+  }, []);
+
+  const scrollToIndex = useCallback((params) => {
+    console.warn('Not supported');
+  }, []);
+
+  const { scrollHandler } = useAnimatedScroll({
+    headerSnappable,
+    scrollTo,
+    scrollToIndex,
+  });
+
+  const handleInternalContentHeight = useCallback((value: number) => {
+    if (mounted.current) {
+      setInternalContentMinHeight(value);
+    }
+  }, []);
+
+  const handleInternalProgressViewOffset = useCallback((value: number) => {
+    if (mounted.current) {
+      setInternalProgressViewOffset(value);
+    }
+  }, []);
+
+  useAnimatedReaction(
+    () => {
+      return contentMinHeight.value;
+    },
+    (result, previous) => {
+      if (result !== previous) {
+        if (
+          contentHeight.current < result &&
+          internalContentMinHeight !== result
+        ) {
+          runOnJS(handleInternalContentHeight)(result);
+        }
+      }
+    }
+  );
+
+  useAnimatedReaction(
+    () => {
+      return fixedHeaderHeight.value;
+    },
+    (result, previous) => {
+      if (result !== previous) {
+        runOnJS(handleInternalProgressViewOffset)(result);
+      }
+    }
+  );
+
+  const contentContainerStyle = useMemo(
+    () => [
+      styles.contentContainer,
+      { minHeight: internalContentMinHeight },
+      props.contentContainerStyle,
+    ],
+    [props.contentContainerStyle, internalContentMinHeight]
+  );
+
+  const handleContentSizeChange = useCallback((_, height) => {
+    contentHeight.current = height;
+  }, []);
+
+  const handleScrollToIndexFailed = useCallback(() => {}, []);
+
+  function renderListHeader() {
+    return (
+      <View>
+        <AnimatedTopView height={headerHeight} />
+        {props.ListHeaderComponent}
+      </View>
+    );
+  }
+
+  return (
+    <AnimatedSectionList
+      ref={scrollViewRef}
+      keyboardDismissMode="on-drag"
+      keyboardShouldPersistTaps="handled"
+      scrollEventThrottle={1}
+      onScrollToIndexFailed={handleScrollToIndexFailed}
+      {...props}
+      style={[styles.container, props.style]}
+      contentContainerStyle={contentContainerStyle}
+      onScroll={scrollHandler}
+      ListHeaderComponent={renderListHeader()}
+      onContentSizeChange={handleContentSizeChange}
+      //@ts-ignore
+      simultaneousHandlers={[]}
+      progressViewOffset={internalProgressViewOffset}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  contentContainer: {
+    flexGrow: 1,
+  },
+  topView: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+  },
+});
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ export { default as useCollapsibleContext } from './hooks/useCollapsibleContext'
 export { default as CollapsibleContainer } from './components/CollapsibleContainer';
 export { default as CollapsibleFlatList } from './components/scrollable/CollapsibleFlatList';
 export { default as CollapsibleScrollView } from './components/scrollable/CollapsibleScrollView';
+export { default as CollapsibleSectionList } from './components/scrollable/CollapsibleSectionList';
 export { default as CollapsibleHeaderContainer } from './components/header/CollapsibleHeaderContainer';
 export { default as StickyView } from './components/header/StickyView';
 export { default as CollapsibleView } from './components/CollapsibleView';
